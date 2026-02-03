842954cbd4c4bb435f284da202208cbc7200617f
Kedar Kawale <kedarkawale003@gmail.com>
2026-02-03 19:47:35 +0530

    day-10 basic practice

---
+++ Day-10/SQLQuery1.sql
@@ -0,0 +1,27 @@
+--Day-10: Basic Query Structure  :[SELECT-FROM-WHERE]
+--Q1- Query to selects all columns for every team member from the ProjectTeam table?	
+/*
+SELECT * 
+FROM ProjectTeam
+GO
+*/
+
+--==============================================
+
+--Q2 You want a list of only team member names from ProjectTeam.
+/*
+SELECT TeamMember
+FROM ProjectTeam
+GO
+*/
+
+--==============================================
+
+--Q3 write a query to select the TeamMember and Role columns for every row in ProjectTeam?
+/*
+SELECT TeamMember, Role
+FROM ProjectTeam
+GO
+*/
+
+--==============================================
