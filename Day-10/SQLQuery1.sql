b0769970947c1973cd4f14dea24e4c5f82857112
Kedar Kawale <kedarkawale003@gmail.com>
2026-02-04 22:47:52 +0530

    Day-10 basic of SELECT-FROM WHERE-DONE

--- Day-10/SQLQuery1.sql
+++ Day-10/SQLQuery1.sql
@@ -25,3 +25,67 @@ GO
 */
 
 --==============================================
+--Q4 Write a  query which returns only the rows where the Role is exactly 'Developer'?
+/*
+SELECT * 
+FROM ProjectTeam
+WHERE Role LIKE '%Developer%';
+GO
+
+or
+
+SELECT *
+FROM ProjectTeam
+WHERE Role = 'Developer'
+GO
+*/
+--==============================================
+--Q5. write a Query to lists TeamMember and Budget only for team members with a Budget greater than 90000?
+/*
+SELECT TeamMember , Budget 
+FROM ProjectTeam
+where Budget > 90000;
+GO
+*/
+--==============================================
+--Q6 Write a query to shows all columns for team members whose Role is either 'Manager' or 'Lead'?
+/*
+SELECT *
+FROM ProjectTeam
+WHERE Role LIKE '%Manager%' OR Role LIKE '%Lead%';
+GO
+
+
+or
+
+SELECT *
+FROM ProjectTeam
+WHERE Role IN('Manager','Lead');
+GO
+*/
+--==============================================
+--Q7 Write a  query to finds all rows where Budget is between 20000 and 80000 (inclusive)?
+/*
+SELECT *
+FROM ProjectTeam
+WHERE Budget BETWEEN 20000 AND 80000;
+GO
+*/
+--==============================================
+--Q8 You need all details for the team member named 'Rashmi Deshmukh'.
+/*
+SELECT * 
+FROM ProjectTeam
+WHERE TeamMember = 'Amit patel'
+GO
+*/
+--==============================================
+
+--Q9 Write a  query that retrieves only the TeamMember and Role for all rows, without any filtering?
+/*
+SELECT TeamMember,Role
+FROM ProjectTeam;
+GO
+*/
+
+--========================================End====================================================
